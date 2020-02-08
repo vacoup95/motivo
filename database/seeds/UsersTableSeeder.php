10258a3ea018c088a5461b579ff9c1e7ae4998f4
Arnold <youtube@minecraftercity.com>
2020-02-08 20:07:46 +0100

    Test user by seeder and composer lock file

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 1)->create()->each(function () {
+            return [
+                'name' => 'Motivo',
+                'email' => 'welkom@motivo.nl',
+                'password' => Hash::make('Test123'),
+                'email_verified_at' => now(),
+            ];
+        });
+    }
+}
